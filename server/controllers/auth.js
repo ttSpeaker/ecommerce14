a394a4c37ee85e6bc4fba1d9d99aaabc6822c331
salvadorserruya <salvador.serruya@kavak.com>
2022-06-13 21:28:14 -0300

    added bcrypt to register/login

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,13 +1,17 @@
+const bcrypt = require('bcrypt');
 const { createUser, findUserByEmail } = require('../models/user');
-
+const salt = 10;
 const registerUser = async (req, res, next) => {
   // save user with model
   try {
     const userBody = req.body;
-    if (await userWithEmailExists(userBody.email)) {
+    if (await searchUserByEmailExists(userBody.email)) {
       res.status(400).json({ message: 'Email already exists' });
       return;
     }
+    //validate password
+    const hash = await bcrypt.hash(userBody.password, salt);
+    userBody.password = hash;
     const newUser = await createUser(userBody);
     res.json(newUser);
   } catch (error) {
@@ -15,13 +19,26 @@ const registerUser = async (req, res, next) => {
   }
 };
 
-const userWithEmailExists = async (email) => {
+const searchUserByEmailExists = async (email) => {
   const user = await findUserByEmail(email);
   return user;
 };
 
 const loginUser = async (req, res, next) => {
-  // verify user with model
+  const userBody = req.body;
+  const user = await searchUserByEmailExists(userBody.email);
+  if (user) {
+    try {
+      const result = await bcrypt.compare(userBody.password, user.password);
+      if (result) {
+        // JWT
+        res.send('LOGGED IN ');
+        return;
+      }
+    } catch (error) {}
+  }
+  res.status(403).json({ message: 'Invalid credentials' });
+  return;
 };
 
 module.exports = { registerUser, loginUser };
